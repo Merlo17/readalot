8b375c3253acf02ec1643ba05aabd4fc7ec54a42
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-11-10 21:26:05 +0200

    feat: skeleton codebase

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,91 +1,192 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-    <header>
-        <img
-            alt="Vue logo"
-            class="logo"
-            src="@/assets/logo.svg"
-            width="125"
-            height="125"
-        />
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
+    <div id="app">
+        <Tinder
+            ref="tinder"
+            key-name="id"
+            :queue.sync="queue"
+            :offset-y="10"
+            @submit="onSubmit"
+        >
+            <template slot-scope="scope">
+                <div
+                    class="pic"
+                    :style="{
+                        'background-image': `url(https://cn.bing.com//th?id=OHR.${scope.data.id}_UHD.jpg&pid=hp&w=720&h=1280&rs=1&c=4&r=0)`,
+                    }"
+                />
+            </template>
+            <img class="like-pointer" slot="like" src="./assets/like-txt.png" />
+            <img class="nope-pointer" slot="nope" src="./assets/nope-txt.png" />
+            <img
+                class="super-pointer"
+                slot="super"
+                src="./assets/super-txt.png"
+            />
+            <img
+                class="rewind-pointer"
+                slot="rewind"
+                src="./assets/rewind-txt.png"
+            />
+        </Tinder>
+        <div class="btns">
+            <img src="./assets/rewind.png" @click="decide('rewind')" />
+            <img src="./assets/nope.png" @click="decide('nope')" />
+            <img src="./assets/super-like.png" @click="decide('super')" />
+            <img src="./assets/like.png" @click="decide('like')" />
+            <img src="./assets/help.png" @click="decide('help')" />
         </div>
-    </header>
-
-    <RouterView />
+    </div>
 </template>
 
-<style scoped>
-header {
-    line-height: 1.5;
-    max-height: 100vh;
+<script>
+import Tinder from 'vue-tinder';
+import source from '@/bing';
+
+export default {
+    name: 'App',
+    components: { Tinder },
+    data: () => ({
+        queue: [],
+        offset: 0,
+        history: [],
+    }),
+    created() {
+        this.mock();
+    },
+    methods: {
+        mock(count = 5, append = true) {
+            const list = [];
+            for (let i = 0; i < count; i++) {
+                list.push({ id: source[this.offset] });
+                this.offset++;
+            }
+            if (append) {
+                this.queue = this.queue.concat(list);
+            } else {
+                this.queue.unshift(...list);
+            }
+        },
+        onSubmit({ item }) {
+            if (this.queue.length < 3) {
+                this.mock();
+            }
+            this.history.push(item);
+        },
+        async decide(choice) {
+            if (choice === 'rewind') {
+                if (this.history.length) {
+                    this.$refs.tinder.rewind([this.history.pop()]);
+                }
+            } else if (choice === 'help') {
+                window.open('https://shanlh.github.io/vue-tinder');
+            } else {
+                this.$refs.tinder.decide(choice);
+            }
+        },
+    },
+};
+</script>
+
+<style>
+html,
+body {
+    height: 100%;
 }
 
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
+body {
+    margin: 0;
+    background-color: #20262e;
+    overflow: hidden;
 }
 
-nav {
+#app .vue-tinder {
+    position: absolute;
+    z-index: 1;
+    left: 0;
+    right: 0;
+    top: 23px;
+    margin: auto;
+    width: calc(100% - 20px);
+    height: calc(100% - 23px - 65px - 47px - 16px);
+    min-width: 300px;
+    max-width: 355px;
+}
+
+.nope-pointer,
+.like-pointer {
+    position: absolute;
+    z-index: 1;
+    top: 20px;
+    width: 64px;
+    height: 64px;
+}
+
+.nope-pointer {
+    right: 10px;
+}
+
+.like-pointer {
+    left: 10px;
+}
+
+.super-pointer {
+    position: absolute;
+    z-index: 1;
+    bottom: 80px;
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: 112px;
+    height: 78px;
+}
+
+.rewind-pointer {
+    position: absolute;
+    z-index: 1;
+    top: 20px;
+    right: 10px;
+    width: 112px;
+    height: 78px;
+}
+
+.pic {
     width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
 }
 
-nav a.router-link-exact-active {
-    color: var(--color-text);
+.btns {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 30px;
+    margin: auto;
+    height: 65px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 300px;
+    max-width: 355px;
 }
 
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
+.btns img {
+    margin-right: 12px;
+    box-shadow: 0 4px 9px rgba(0, 0, 0, 0.15);
+    border-radius: 50%;
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
 }
 
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
+.btns img:nth-child(2n + 1) {
+    width: 53px;
 }
 
-nav a:first-of-type {
-    border: 0;
+.btns img:nth-child(2n) {
+    width: 65px;
 }
 
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
-    }
+.btns img:nth-last-child(1) {
+    margin-right: 0;
 }
 </style>
