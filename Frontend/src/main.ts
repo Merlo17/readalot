e0341dd2a52c309d45fadfd93feb9c05765f6f97
Merlo17 <matteo.merler@gmail.com>
2023-11-11 23:30:18 +0200

    paper scroller

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -7,11 +7,12 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import { faHeart, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartReg, faCircleXmark as faCircleXmarkReg } from '@fortawesome/free-regular-svg-icons';
 
 import App from './App.vue';
 import router from './router';
 
-library.add(faHeart, faCircleXmark);
+library.add(faHeart, faCircleXmark, faHeartReg, faCircleXmarkReg);
 
 const app = createApp(App);
 
