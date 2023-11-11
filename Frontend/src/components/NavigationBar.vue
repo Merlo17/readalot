b872ed4f937e0080c5e2ef4c5966e75a1bc76c56
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-11-11 19:33:24 +0200

    feat: navigation bar centered

--- Frontend/src/components/NavigationBar.vue
+++ Frontend/src/components/NavigationBar.vue
@@ -1,15 +1,20 @@
 <template>
     <main
-        class="text-white flex flex-row justify-between w-full py-2 px-4 bg-slate-800"
+        class="text-white flex flex-row justify-between w-full py-4 px-8 bg-slate-800 place-items-center"
     >
-        <div class="flex flex-row gap-2">
-            <p>Logo</p>
-            <p class="font-bold">PaperMatch</p>
+        <div class="w-1/3 justify-start">
+            <img src="../assets/img/logo_white.png" class="h-16" alt="Logo" />
         </div>
-        <div class="w-1/4">
+        <div class="w-1/3 justify-center">
             <QueryBar v-model="query" />
         </div>
-        <p>Account pic</p>
+        <div class="w-1/3 flex justify-end">
+            <img
+                src="../assets/img/profile.jpg"
+                class="rounded-full w-16 h-16"
+                alt="Profile"
+            />
+        </div>
     </main>
 </template>
 
